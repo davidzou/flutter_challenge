ec6ab1a1c19b0ca30e441b7f6f89b368da8a1a25
zouzhangwei <wearecisco@gmail.com>
2020-09-06 23:25:18 +0800

    add new widgets 004 animatedcontainer

---
+++ f_004_widgets_animatedcontainer/android/app/src/main/kotlin/com/wonderingwall/f_004_widgets_animatedcontainer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.wonderingwall.f_004_widgets_animatedcontainer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ f_004_widgets_animatedcontainer/lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  bool selected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body:
+         GestureDetector(
+          onTap: () {
+            setState(() {
+              selected = !selected;
+            });
+          },
+          child: Center(
+            child: AnimatedContainer(
+              width: selected ? 200.0 : 100.0,
+              height: selected ? 100.0 : 200.0,
+              color: selected ? Colors.red : Colors.blue,
+              alignment: selected ? Alignment.center : AlignmentDirectional.topCenter,
+              duration: Duration(milliseconds: 500),
+              curve: Curves.fastOutSlowIn,
+              child: FlutterLogo(size: 75),
+            ),
+          ),
+        ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
