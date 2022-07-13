12c489dd9da9f93e82b810432a27b8cb1df6f3be
zouzhangwei <wearecisco@gmail.com>
2022-07-13 11:39:15 +0800

    UPDATE: update f_002_widgets_wrap that used plume version 0.1.4 and null-safty to support.

--- f_002_widgets_wrap/lib/main.dart
+++ f_002_widgets_wrap/lib/main.dart
@@ -2,8 +2,10 @@ import 'package:f_002_widgets_wrap/wrap_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "",
-    home: WrapPage(),
-  ));
+  runApp(
+    MaterialApp(
+      title: "f_002_widgets_wrap",
+      home: WrapPage(),
+    ),
+  );
 }
